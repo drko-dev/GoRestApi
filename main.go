19ea33f63a7d9d7a281368e52dfa03763bdca7d5
gustavo <gustavo@mail.com>
2018-03-28 21:24:00 -0300

    data on server new fix test4

--- main.go
+++ main.go
@@ -3,34 +3,33 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 	"rest-api/route"
 	"time"
 )
 
 func main() {
 
-	var chain, key, port string
-	if len(os.Args) > 1 {
-		chain = os.Args[1]
-		key = os.Args[2]
-		port = os.Args[3]
-	} else {
-		chain = "/etc/ssl/certs/fullchain.pem"
-		key = "/etc/ssl/certs/privkey.pem"
-		port = "1700"
-	}
+	/* 	var chain, key, port string
+	   	if len(os.Args) > 1 {
+	   		chain = os.Args[1]
+	   		key = os.Args[2]
+	   		port = os.Args[3]
+	   	} else {
+	   		chain = "/etc/ssl/certs/fullchain.pem"
+	   		key = "/etc/ssl/certs/privkey.pem"
+	   		port = "1700"
+	   	} */
 
 	router := route.NewMuxRouter()
 
 	server := &http.Server{
-		Addr:           ":" + port,
+		Addr:           ":1700",
 		Handler:        router,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	log.Println("Escuchando puerto " + port + " ...")
-	log.Println(server.ListenAndServeTLS(chain, key))
+	log.Println("Escuchando puerto 1700 ...")
+	log.Println(server.ListenAndServe())
 }
